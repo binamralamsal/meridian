77f0e2565c4843e3adda0beab1834068b4732168
binamralamsal <binamralamsal@gmail.com>
2025-03-13 21:29:28 +0545

    Shadcn button, input, color setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,3 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-
 export default function Home() {
-  return (
-    <>
-      <Input className="max-w-4xl" />
-      <Button>Get Started</Button>
-    </>
-  );
+  return <></>;
 }
