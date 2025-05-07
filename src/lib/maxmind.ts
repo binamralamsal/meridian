da4de6f59ae3733a08c301406017892342103a5e
binamralamsal <binamralamsal@gmail.com>
2025-05-07 16:10:13 +0545

    trying to fix some errors

--- src/lib/maxmind.ts
+++ src/lib/maxmind.ts
@@ -1,13 +1,11 @@
 import maxmind, { CityResponse } from "maxmind";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export const openMaxmind = () =>
   maxmind.open<CityResponse>(
-    path.join(
-      import.meta.dirname,
-      "..",
-      "..",
-      ".maxmind",
-      "GeoLite2-City.mmdb",
-    ),
+    path.join(__dirname, "..", "..", ".maxmind", "GeoLite2-City.mmdb"),
   );
