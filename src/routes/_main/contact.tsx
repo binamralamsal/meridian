c05bb087ebe986b99b3fd7aca81501000decf4f6
binamralamsal <binamralamsal@gmail.com>
2025-06-29 13:18:57 +0545

    map fix

--- src/routes/_main/contact.tsx
+++ src/routes/_main/contact.tsx
@@ -120,7 +120,7 @@ function RouteComponent() {
 
       <section className="w-full">
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3531.3090306846375!2d85.33298177546851!3d27.73861277616474!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb1947a2746d3b%3A0x8e1b4bf90fd7f4e!2sMeridian%20Health%20care%20Center%20Pvt.%20Ltd.!5e0!3m2!1sen!2snp!4v1713343133788!5m2!1sen!2snp"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d524.9320811370936!2d85.33575134757635!3d27.73864327334833!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb1900452543bf%3A0x71c15277c20eb9b1!2sMeridian%20Multi%20Speciality%20Polyclinic%20Pvt.%20Ltd!5e0!3m2!1sen!2snp!4v1751182356399!5m2!1sen!2snp"
           className="h-[400px] w-full border-0"
           allowFullScreen
           loading="lazy"
